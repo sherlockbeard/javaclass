8e2e7e3ee4a5d71154ad1cecec664375943d7928
sherlock <b417044@iiit-bh.ac.in>
2019-09-04 16:09:40 +0530

    nice69420

--- src/quest_10/quest10_class2.java
+++ src/quest_10/quest10_class2.java
@@ -5,10 +5,34 @@ import java.util.Scanner;
 public class quest10_class2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String a = scanner.next();
-        String b = change(a);
+        String a = scanner.nextLine();
+        System.out.println(change(a));
     }
     public static String change(String a){
-        int i =0;
+        String b = "";
+        char newnow ;
+        for(int i=0;i<a.length();i++){
+           char now = a.charAt(i);
+           if(now>='a' && now<='z'){
+               int nowa = now + 13;
+               if(nowa>'z'){
+                   newnow = (char) ((nowa - 'z')+'a'-1);
+               }else{
+                   newnow = (char) nowa;
+               }
+               b+= newnow;
+           }else if(now>='A' && now<='Z'){
+               int nowa = now + 13;
+               if(nowa>'Z'){
+                   newnow = (char) ((nowa - 'Z')+'A'-1);
+               }else{
+                   newnow = (char) nowa;
+               }
+               b+=newnow;
+           }else if(now == ' '){
+               b+=now;
+           }
+        }
+        return b;
     }
 }
